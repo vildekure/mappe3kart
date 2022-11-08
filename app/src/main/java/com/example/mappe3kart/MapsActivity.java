f12421b7481395f44241dcb9693cc6d1ce79d629
Vilde <69961376+vildekure@users.noreply.github.com>
2022-11-08 12:40:39 +0100

    lagt til async fra forelesning

--- app/src/main/java/com/example/mappe3kart/MapsActivity.java
+++ app/src/main/java/com/example/mappe3kart/MapsActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,6 +15,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.mappe3kart.databinding.ActivityMapsBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
     private GoogleMap mMap;
@@ -30,6 +40,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        getJSON task = new getJSON();
+        task.execute(new
+                String[]{"http://data1500.cs.oslomet.no/~s354592/jsonout.php"});
+
     }
 
     @Override
@@ -67,5 +82,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .title("Test Marker");
         mMap.addMarker(marker);
 
+    }
+
+    private class getJSON extends AsyncTask<String, Void, String> {
+        JSONObject jsonObject;
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String s = "";
+            String output = "";
+
+            for (String url : urls) {
+                try {
+                    URL urlen = new URL(urls[0]);
+                    HttpURLConnection conn = (HttpURLConnection)
+                            urlen.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setRequestProperty("Accept",
+                            "application/json");
+                    if (conn.getResponseCode() != 200) {
+                        throw new RuntimeException("Failed : HTTP error code : "
+                                + conn.getResponseCode());
+                    }
+                    BufferedReader br = new BufferedReader(new InputStreamReader(
+                            (conn.getInputStream())));
+                    System.out.println("Output from Server .... \n");
+                    while ((s = br.readLine()) != null) {
+                        output = output + s;
+                    }
+                    conn.disconnect();
+                    try {
+                        JSONArray mat = new JSONArray(output);
+                        for (int i = 0; i < mat.length(); i++) {
+                            JSONObject jsonobject = mat.getJSONObject(i);
+
+                            String latitude = jsonobject.getString("latitude");
+                            String longitude = jsonobject.getString("longitude");
+                            String name = jsonobject.getString("name");
+                            String info = jsonobject.getString("info");
+                            String adresse = jsonobject.getString("adresse");
+
+                        }
+                        return null;
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                } catch (Exception e) {
+                    return "Noe gikk galt";
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String ss) {
+
+        }
     }
 }
