10d19d7647faaad93c35adc032c4ca279a03b1d5
Vilde <69961376+vildekure@users.noreply.github.com>
2022-11-08 11:39:15 +0100

    Pojo endret, lagt til info igjen på edit xml

--- app/src/main/java/com/example/mappe3kart/Models/Marker.java
+++ app/src/main/java/com/example/mappe3kart/Models/Marker.java
@@ -5,21 +5,27 @@ public class Marker {
     private String latitude;
     private String longitude;
     private String name;
+    private String info;
+    private String adresse;
 
     public Marker(){
     }
 
-    public Marker(String latitude, String longitude, String name) {
+    public Marker(String latitude, String longitude, String name, String info, String adresse) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.name = name;
+        this.info = info;
+        this.adresse = adresse;
     }
 
-    public Marker(int ID, String latitude, String longitude, String name) {
+    public Marker(int ID, String latitude, String longitude, String name, String info, String adresse) {
         this.ID = ID;
         this.latitude = latitude;
         this.longitude = longitude;
         this.name = name;
+        this.info = info;
+        this.adresse = adresse;
     }
 
     public int getID() {
@@ -46,12 +52,27 @@ public class Marker {
         this.longitude = longitude;
     }
 
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
     }
 }
